033839c2cfd0c7e39a4ae091369e5e6a7f5466ab
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-02-23 19:53:31 +0530

    added welcome file

---
+++ src/employewage/UC0.java
@@ -0,0 +1,10 @@
+package employewage;
+
+public class UC0 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation");
+		System.out.println("Using Oops Concept");
+	}
+
+}
