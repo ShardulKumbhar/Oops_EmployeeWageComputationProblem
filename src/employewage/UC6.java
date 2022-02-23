24fa265dc8e2e7db5b3e012cb71cd9bcf41d7b3c
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-02-23 19:50:10 +0530

    added uc6 file

--- src/employewage/UC6.java
+++ src/employewage/UC6.java
@@ -2,20 +2,21 @@ package employewage;
 
 class employee6 {
 	void monthly() {
-		// variable assigne
+		// variable assign
 		int empRatePerHr = 20;
 		double totalsalary = 0;
-		double salary = 0;
-		int numofworkingDays = 20;
+		int maxRateInMonth = 100;
+		int numOfWorkingDays = 20;
+		int totalEmpHr = 0;
+		int totalWorkingDays = 0;
 		int empHrs = 0;
-		int day = 1;
 
-		for (day = 1; day <= numofworkingDays; day++) {
-
+		// to check maximum rate in month and num of days
+		while (totalEmpHr <= maxRateInMonth && totalWorkingDays <= numOfWorkingDays) {
+			totalWorkingDays++;
 			double randomCheck = Math.floor(Math.random() * 10) % 3;
 			int i = (int) randomCheck;
 			switch (i) {
-
 			case 2:
 				System.out.println("Employee is FullTime");
 				empHrs = 8;
@@ -30,19 +31,21 @@ class employee6 {
 				break;
 			}
 
-			salary = empHrs * empRatePerHr;
-			totalsalary = totalsalary + salary;
-			System.out.println("Employee this month Salary :" + totalsalary);
+			totalEmpHr = totalEmpHr + empHrs;
+			System.out.println(totalEmpHr);
 		}
+		totalsalary = totalEmpHr * empRatePerHr;
+
+		System.out.println("Employee total Salary" + totalsalary);
 	}
 }
 
-public class UC5 {
+public class UC6 {
 
 	public static void main(String[] args) {
 		// creating object
 		employee6 r = new employee6();
-		// calling object by methord
+		// calling methord
 		r.monthly();
 	}
 
